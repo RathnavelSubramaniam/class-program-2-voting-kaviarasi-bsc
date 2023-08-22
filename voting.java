cceb7a446424cf4826da119f197e53b64a5525b6
kaviarasi-bsc <142780143+kaviarasi-bsc@users.noreply.github.com>
2023-08-22 09:42:16 +0000

    completed

---
+++ voting.java
@@ -0,0 +1,11 @@
+class vote{
+    public static void main(String args[]){
+        int a=17;
+        if(a<=18){
+            System.out.println("NOT ELIGIBLE");
+        }
+        else{
+            System.out.println("ELIGIBLE");
+        }
+    }
+}
